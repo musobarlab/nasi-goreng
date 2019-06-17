0e67400fa29aedc5d4bd9f26c09798b7501db356
wuriyanto48 <wuriyanto48@yahoo.co.id>
2019-06-17 14:08:35 +0700

    add validation

--- scripts/bitbucket_private_repo.sh
+++ scripts/bitbucket_private_repo.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+BITBUCKET_ID=wuriyanto
+BITBUCKET_TOKEN=$1
+
+if [[ -z "$BITBUCKET_TOKEN" ]]; then
+    echo "$0 require personal access token"
+    echo "example : ./bitbucket_private_repo.sh 7djeksksuej"
+    exit 1
+fi
+
 git config \
   --global \
-  url."https://${bitbucket_id}:${bitbucket_token}@privatebitbucket.com".insteadOf \
+  url."https://${BITBUCKET_ID}:${BITBUCKET_TOKEN}@privatebitbucket.com".insteadOf \
   "https://privatebitbucket.com"
--- scripts/gitlab_private_repo.sh
+++ scripts/gitlab_private_repo.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+USERNAME=wuriyanto
+PERSONAL_ACCESS_TOKEN=$1
+
+if [[ -z "$PERSONAL_ACCESS_TOKEN" ]]; then
+    echo "$0 require personal access token"
+    echo "example : ./gitlab_private_repo.sh 7djeksksuej"
+    exit 1
+fi
+
 git config \
   --global \
-  url."https://${user}:${personal_access_token}@privategitlab.com".insteadOf \
+  url."https://${USERNAME}:${PERSONAL_ACCESS_TOKEN}@privategitlab.com".insteadOf \
   "https://privategitlab.com"
